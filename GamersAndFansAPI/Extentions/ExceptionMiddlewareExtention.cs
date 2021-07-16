16c48043b47abc94a6cc3bb44e36cc115aceef0a
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-16 22:55:05 +0200

    Configure logger manager

---
+++ GamersAndFansAPI/Extentions/ExceptionMiddlewareExtention.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contract;
+using Microsoft.AspNetCore.Builder;
+
+namespace GamersAndFansAPI.Extentions
+{
+    public static class ExceptionMiddlewareExtention
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder applicationBuilder , ILoggerManager loggerManager)
+        {
+
+        }
+    }
+}
