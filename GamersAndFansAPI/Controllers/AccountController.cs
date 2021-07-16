9d4b73b26831ecbdde45bb4ca458cb87d7d5bfd6
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-16 21:55:56 +0200

    Configure dependancy injection

--- GamersAndFansAPI/Controllers/AccountController.cs
+++ GamersAndFansAPI/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Contract.Authentication;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +15,21 @@ namespace GamersAndFansAPI.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IMapper Mapper;
+        private readonly UserManager<User> UserManager;
+        private readonly RoleManager<IdentityRole> RoleManager;
+        private readonly IAuthenticationManager AuthenticationManager;
+
+        public AccountController(IMapper mapper,UserManager<User>userManager,RoleManager<IdentityRole> roleManager,
+                                  IAuthenticationManager authenticationManager)
+        {
+            Mapper = mapper;
+            UserManager = userManager;
+            RoleManager = roleManager;
+            AuthenticationManager = authenticationManager;
+
+        }
+
+
     }
 }
