9c3a3f77d6985b4614c149657a6560d06c7ddbb5
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-17 00:26:21 +0200

    Configure SQL Server in Startup.cs file

--- GamersAndFansAPI/Startup.cs
+++ GamersAndFansAPI/Startup.cs
@@ -1,7 +1,9 @@
 using Contract;
+using Entities.Context;
 using GamersAndFansAPI.Extentions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,9 @@ namespace GamersAndFansAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "GamersAndFansAPI", Version = "v1" });
             });
+
+            services.AddDbContext<GamerDb>(
+                 options => options.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
