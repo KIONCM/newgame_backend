80e8f7e76df9203e6b659fbc211154ae11a9bf03
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-18 22:43:55 +0200

    Build IScoreRepository
    The IScoreRepository is inherits from the base and it's built to communicate with database

---
+++ Repositories/IRepository/IScoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.IRepository
+{
+    public interface IScoreRepository
+    {
+        public Task<IEnumerable<Score>> ListAsync();
+        public Task<Score>FindByUserIdAsync(string UserId);
+        public Task<Score>AddAsync(Score score);
+        public void UpdateAsync(Score score);
+        public void DeleteAsync(Score score);
+
+    }
+}
