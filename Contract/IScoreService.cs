ced1ef7a4b1a526288c78a39fe62db724eb30bde
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-18 22:29:34 +0200

    Create the Interface IScoreService
    It's to implement the scores logic and  handle operations

---
+++ Contract/IScoreService.cs
@@ -0,0 +1,18 @@
+﻿using Contract.Communications;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public interface IScoreService
+    {
+        public Task<IEnumerable<Score>> RetriveTheListOfScores();
+        public Task<ScoreResponce> ListScoresByUserId(string UserId);
+        public Task<ScoreResponce> UpdateScoreBasedOnUserId(string UserId,Score score);
+        public Task<ScoreResponce> DeleteScoreBasedOnUserId(string UserId);
+    }
+}
