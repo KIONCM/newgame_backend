0ed2967fe9ce45eb604ed8b7012228177cf582f0
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-18 21:36:50 +0200

    Identify the ScoresDTO for retriving data

---
+++ Entities/DataTransfareObjects/Retrive/ScoresDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entities.DataTransfareObjects.Retrive
+{
+    public class ScoresDTO
+    {
+        public Guid Id { get; set; }
+        public int Scores { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
