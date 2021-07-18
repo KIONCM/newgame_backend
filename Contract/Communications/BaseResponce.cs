1a449ba885f51c0bea2d79d23c3bb263629828b5
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-18 22:26:09 +0200

    Move the Commucation Layer to the contract lib

---
+++ Contract/Communications/BaseResponce.cs
@@ -0,0 +1,14 @@
+﻿namespace Contract.Communications
+{
+    public abstract class BaseResponce
+    {
+        public bool Success { get; protected set; }
+        public string Message { get; protected set; }
+
+        public BaseResponce(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
---
+++ Contract/Communications/ScoreResponce.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using GamersAndFansAPI.Domain.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contract.Communications
+{
+    public class ScoreResponce : BaseResponce
+    {
+        public Score Score{ get; set; }
+        public ScoreResponce(bool success, string message,Score score) : base(success, message)
+        {
+            Score = score;
+        }
+
+        public ScoreResponce(Score score):this(true,string.Empty,score)
+        {
+
+        }
+
+        public ScoreResponce(string message):this(false,message,null)
+        {
+
+        }
+    }
+}
