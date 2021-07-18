995d32b6bc901abfeedd68cf85a7c5b22e3cc6e7
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-18 22:34:06 +0200

    build Abstract BaseRepository
    The BaseRepository is to inject the Database into repositories

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly GamerDb Context;
+        public BaseRepository(GamerDb context)
+        {
+            Context = context;
+        }
+    }
+}
