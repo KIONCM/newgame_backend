ae3c4cc4a39a90ea9c39eaf0d1bee388bd71c386
Sam-Rex <osamaabubaker111@gmail.com>
2021-07-19 23:49:44 +0200

    Creating ModelStateExtemtion
    Used to register the errors messages inside controllers

---
+++ GamersAndFansAPI/Extentions/ModelStateExtention.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extentions
+{
+    public static class ModelStateExtention
+    {
+        public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+        {
+            return dictionary.SelectMany(m => m.Value.Errors)
+                .Select(m => m.ErrorMessage).ToList();
+        }
+    }
+}
